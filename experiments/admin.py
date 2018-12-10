7d0435165229d2312b009799ccdc8d15286105ed
Fernando Oliveira <nandooliveira.al@gmail.com>
2018-12-10 02:42:46 -0300

    new data model

--- experiments/admin.py
+++ experiments/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from experiments.models import Participant
 
 class ParticipantAdmin(admin.ModelAdmin):
-    list_filter = ('start_datetime',)
-    list_display = ('name', 'email', 'start_datetime', 'end_datetime', 'task1_start', 'task1_end', 'task2_start', 'task2_end')
+    list_filter = ('created_at',)
+    list_display = ('name', 'email', 'created_at', 'updated_at')
 
 admin.site.register(Participant, ParticipantAdmin)
--- experiments/models.py
+++ experiments/models.py
@@ -3,21 +3,40 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class Experiment(models.Model):
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Participant(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    flow = models.CharField(max_length=200)
-    start_datetime = models.DateTimeField(null=True, blank=True)
-    end_datetime = models.DateTimeField(null=True, blank=True)
-    task1_start = models.DateTimeField(null=True, blank=True)
-    task1_end = models.DateTimeField(null=True, blank=True)
-    task2_start = models.DateTimeField(null=True, blank=True)
-    task2_end = models.DateTimeField(null=True, blank=True)
-    task1_data = models.TextField(null=True, blank=True)
-    task2_data = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return self.name
+
+
+def task_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/uploads/task_<id>/<filename>
+    return 'uploads/task_{0}/{1}'.format(instance.user.id, filename)
+
+class Task(models.Model):
+    description = models.CharField(max_length=200)
+    image = models.FileField(upload_to=task_directory_path)
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Execution(models.Model):
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE, blank=True, null=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, blank=True, null=True)
+    start = models.DateTimeField(null=True, blank=True)
+    end = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Point(models.Model):
     x = models.CharField(max_length=200)
