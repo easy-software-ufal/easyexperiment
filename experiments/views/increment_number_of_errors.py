14747d302b2162fc0c3cd64a3d0262a8c1fcbf4c
Fernando Oliveira <nandooliveira.al@gmail.com>
2019-03-28 15:00:38 -0300

    refactoring

--- experiments/views/increment_number_of_errors.py
+++ experiments/views/increment_number_of_errors.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+from django.db.models import F
 from django.forms.models import model_to_dict
 from django.views.generic import View
 from django.http import HttpResponse
@@ -16,9 +17,7 @@ class IncrementNumberOfErrors(View):
         return HttpResponse(json.dumps(result), **response_kwargs)
 
     def increment_number_of_errors(self, execution_id):
-        execution = Execution.objects.get(pk=execution_id)
-        execution.number_of_errors += 1
-        execution.save()
-        execution.refresh_from_db()
+        Execution.objects.filter(pk=execution_id).update(number_of_errors=F('number_of_errors')+1)
 
+        execution = Execution.objects.get(pk=execution_id)
         return execution.number_of_errors
